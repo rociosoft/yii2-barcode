0025bfcaca74b572851eda1224aacafa2ebd6de5
Yasniel Machín Hernández <yasniel@gmail.com>
2021-05-17 17:48:32 -0500

    - Set filename and label properties

--- GeneratedCodebar.php
+++ GeneratedCodebar.php
@@ -19,59 +19,82 @@ include_once('class' . DIRECTORY_SEPARATOR . 'BCGcode128.barcode.php');
  */
 class GeneratedCodebar extends \yii\base\Widget
 {
-    public $message;
 
-    public function init()
-    {
-        parent::init();
-        if ($this->message == null) {
-            throw new Exception('wrong message');
-        }
-    }
+	public $message;
+	public $filename;
+	public $label = '';
 
-    public function run()
-    {
-/*        $className = 'BCGcodabar';
-        $baseClassFile = 'BCGBarcode1D.php';
-        $codeVersion = '5.0.2';*/
+	public function init()
+	{
+		parent::init();
+		if ($this->message == null) {
+			throw new Exception('wrong message');
+		}
+		if ($this->filename == null) {
+			throw new Exception('wrong filename');
+		}
+	}
 
-        $color_black = new \BCGColor(0, 0, 0);
-        $color_white = new \BCGColor(255, 255, 255);
+	public function run()
+	{
+		/*        $className = 'BCGcodabar';
+				$baseClassFile = 'BCGBarcode1D.php';
+				$codeVersion = '5.0.2';*/
 
-        $filetypes = array('PNG' => \BCGDrawing::IMG_FORMAT_PNG, 'JPEG' => \BCGDrawing::IMG_FORMAT_JPEG, 'GIF' => \BCGDrawing::IMG_FORMAT_GIF);
+		$color_black = new \BCGColor(0, 0, 0);
+		$color_white = new \BCGColor(255, 255, 255);
 
-        $code_generated = new \BCGcode128();
+		$filetypes = array('PNG' => \BCGDrawing::IMG_FORMAT_PNG, 'JPEG' => \BCGDrawing::IMG_FORMAT_JPEG, 'GIF' => \BCGDrawing::IMG_FORMAT_GIF);
 
-        $this->baseCustomSetup($code_generated);
+		$code_generated = new \BCGcode128();
 
-        $code_generated->setScale(max(1, min(4, 4)));
-        $code_generated->setBackgroundColor($color_white);
-        $code_generated->setForegroundColor($color_black);
-        $code_generated->parse($this->message);
+		$this->baseCustomSetup($code_generated);
 
-        $drawing = new \BCGDrawing('', $color_white);
-        $drawing->setBarcode($code_generated);
-        $drawing->setRotationAngle(0);
-        $drawing->setDPI(72);
-        $drawing->draw();
-        header('Content-Type: image/png');
-        $drawing->finish($filetypes['PNG']);
-        return "Hello!";
-    }
+		$code_generated->setScale(max(1, min(4, 4)));
+		$code_generated->setBackgroundColor($color_white);
+		$code_generated->setForegroundColor($color_black);
+		$code_generated->parse($this->message);
 
-    public function baseCustomSetup($barcode)
-    {
-        $font_dir =  'font';
+		if(!empty($this->label))
+		{
+			$fontLabel = new \BCGFontFile(__DIR__.'/font/Arial.ttf', 25);
+			$label = new \BCGLabel($this->label, $fontLabel, 'POSITION_TOP');
+			$code_generated->addLabel($label);
+		}
 
-        $barcode->setThickness(max(9, min(90, 25)));
-        $font = new \BCGFontFile(__DIR__.'/font/Arial.ttf', 15);
+		$drawing = new \BCGDrawing('', $color_white);
+		$drawing->setFilename($this->filename);
+		$drawing->setBarcode($code_generated);
+		$drawing->setRotationAngle(0);
+		$drawing->setDPI(72);
+		$drawing->draw();
 
-        $barcode->setFont($font);
-    }
+//        $drawing = new BCGDrawing('', $color_white);
+//        $drawing->setFilename("XMLs/".$text.".png");
+//        if($drawException) {
+//            $drawing->drawException($drawException);
+//        } else {
+//            $drawing->setBarcode($code);
+//            $drawing->draw();
+//        }
+		//header('Content-Type: image/png');
+		$drawing->finish($filetypes['PNG']);
+		//return "Hello!";
+	}
 
-/*    public function customSetup($barcode, $get) {
-        if (isset($get['checksum'])) {
-            $barcode->setChecksum($get['checksum'] === '1' ? true : false);
-        }
-    }*/
+	public function baseCustomSetup($barcode)
+	{
+		$font_dir =  'font';
+
+		$barcode->setThickness(max(9, min(90, 25)));
+		$font = new \BCGFontFile(__DIR__.'/font/Arial.ttf', 35);
+
+		$barcode->setFont($font);
+	}
+
+	/*    public function customSetup($barcode, $get) {
+			if (isset($get['checksum'])) {
+				$barcode->setChecksum($get['checksum'] === '1' ? true : false);
+			}
+		}*/
 }
